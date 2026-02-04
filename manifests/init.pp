f3076b3dc55ea0a81adff5891fcdbc912d7e9fed
Chris Allen <chris.j.allen1979@gmail.com>
2026-02-04 12:49:14 +0000

    Automated conversion from geerlingguy.kibana Ansible role

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,6 +13,7 @@
 # @param kibana_service_enabled
 # @param kibana_config_template
 # @param kibana_config_file_path
+# @param par_vardir Base directory for Puppet agent cache (uses lookup('paw::par_vardir') for common config)
 # @param par_tags An array of Ansible tags to execute (optional)
 # @param par_skip_tags An array of Ansible tags to skip (optional)
 # @param par_start_at_task The name of the task to start execution at (optional)
@@ -37,6 +38,7 @@ class paw_ansible_role_kibana (
   Boolean $kibana_service_enabled = true,
   String $kibana_config_template = 'kibana.yml.j2',
   String $kibana_config_file_path = '/etc/kibana/kibana.yml',
+  Optional[Stdlib::Absolutepath] $par_vardir = undef,
   Optional[Array[String]] $par_tags = undef,
   Optional[Array[String]] $par_skip_tags = undef,
   Optional[String] $par_start_at_task = undef,
@@ -50,42 +52,41 @@ class paw_ansible_role_kibana (
   Optional[Boolean] $par_exclusive = undef
 ) {
 # Execute the Ansible role using PAR (Puppet Ansible Runner)
-  $vardir = $facts['puppet_vardir'] ? {
-    undef   => $settings::vardir ? {
-      undef   => '/opt/puppetlabs/puppet/cache',
-      default => $settings::vardir,
-    },
-    default => $facts['puppet_vardir'],
-  }
-  $playbook_path = "${vardir}/lib/puppet_x/ansible_modules/ansible_role_kibana/playbook.yml"
+# Playbook synced via pluginsync to agent's cache directory
+# Check for common paw::par_vardir setting, then module-specific, then default
+$_par_vardir = $par_vardir ? {
+  undef   => lookup('paw::par_vardir', Stdlib::Absolutepath, 'first', '/opt/puppetlabs/puppet/cache'),
+  default => $par_vardir,
+}
+$playbook_path = "${_par_vardir}/lib/puppet_x/ansible_modules/ansible_role_kibana/playbook.yml"
 
-  par { 'paw_ansible_role_kibana-main':
-    ensure        => present,
-    playbook      => $playbook_path,
-    playbook_vars => {
-      'kibana_version'                => $kibana_version,
-      'kibana_server_port'            => $kibana_server_port,
-      'kibana_server_host'            => $kibana_server_host,
-      'kibana_elasticsearch_url'      => $kibana_elasticsearch_url,
-      'kibana_elasticsearch_username' => $kibana_elasticsearch_username,
-      'kibana_elasticsearch_password' => $kibana_elasticsearch_password,
-      'kibana_package'                => $kibana_package,
-      'kibana_package_state'          => $kibana_package_state,
-      'kibana_service_state'          => $kibana_service_state,
-      'kibana_service_enabled'        => $kibana_service_enabled,
-      'kibana_config_template'        => $kibana_config_template,
-      'kibana_config_file_path'       => $kibana_config_file_path,
-    },
-    tags          => $par_tags,
-    skip_tags     => $par_skip_tags,
-    start_at_task => $par_start_at_task,
-    limit         => $par_limit,
-    verbose       => $par_verbose,
-    check_mode    => $par_check_mode,
-    timeout       => $par_timeout,
-    user          => $par_user,
-    env_vars      => $par_env_vars,
-    logoutput     => $par_logoutput,
-    exclusive     => $par_exclusive,
-  }
+par { 'paw_ansible_role_kibana-main':
+  ensure        => present,
+  playbook      => $playbook_path,
+  playbook_vars => {
+        'kibana_version' => $kibana_version,
+        'kibana_server_port' => $kibana_server_port,
+        'kibana_server_host' => $kibana_server_host,
+        'kibana_elasticsearch_url' => $kibana_elasticsearch_url,
+        'kibana_elasticsearch_username' => $kibana_elasticsearch_username,
+        'kibana_elasticsearch_password' => $kibana_elasticsearch_password,
+        'kibana_package' => $kibana_package,
+        'kibana_package_state' => $kibana_package_state,
+        'kibana_service_state' => $kibana_service_state,
+        'kibana_service_enabled' => $kibana_service_enabled,
+        'kibana_config_template' => $kibana_config_template,
+        'kibana_config_file_path' => $kibana_config_file_path
+              },
+  tags          => $par_tags,
+  skip_tags     => $par_skip_tags,
+  start_at_task => $par_start_at_task,
+  limit         => $par_limit,
+  verbose       => $par_verbose,
+  check_mode    => $par_check_mode,
+  timeout       => $par_timeout,
+  user          => $par_user,
+  env_vars      => $par_env_vars,
+  logoutput     => $par_logoutput,
+  exclusive     => $par_exclusive,
+}
 }
